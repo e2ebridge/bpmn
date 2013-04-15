ded5616351ab479845eb2b3ef9fdd7cd3320dcdd
Marcel Rassinger <mrassinger@e2e.ch>
2013-04-15 15:08:49 +0200

    Added catch timeout event.

--- test/scripts/process/timoutClearingByLeavingTask.test.js
+++ test/scripts/process/timoutClearingByLeavingTask.test.js
@@ -1,8 +1,3 @@
-/**
- * AUTHOR: mrassinger
- * COPYRIGHT: E2E Technologies Ltd.
- */
-
 var BPMNProcessDefinition = require('../../../lib/bpmn/processDefinition.js').BPMNProcessDefinition;
 var BPMNProcess = require('../../../lib/process.js').BPMNProcess;
 var BPMNTask = require("../../../lib/bpmn/tasks.js").BPMNTask;
@@ -11,22 +6,24 @@ var BPMNEndEvent = require("../../../lib/bpmn/endEvents.js").BPMNEndEvent;
 var BPMNSequenceFlow = require("../../../lib/bpmn/sequenceFlows.js").BPMNSequenceFlow;
 var BPMNBoundaryEvent = require("../../../lib/bpmn/boundaryEvents.js").BPMNBoundaryEvent;
 
-exports.testBPMNTimeout = function(test) {
+exports.testClearBPMNTimeoutByLeavingTask = function(test) {
     var boundaryEvent = new BPMNBoundaryEvent("_7", "MyTimeout", "boundaryEvent", "_3", [], ["_8"]);
     boundaryEvent.isTimerEvent = true;
 
     /** @type {BPMNProcessDefinition} */
     var processDefinition = new BPMNProcessDefinition("PROCESS_1", "myProcess");
     processDefinition.addFlowObject(new BPMNStartEvent("_2", "MyStart", "startEvent", [], ["_4"]));
-    processDefinition.addFlowObject(new BPMNTask("_3", "MyTask", "task", ["_4"], []));
+    processDefinition.addFlowObject(new BPMNTask("_3", "MyTask", "task", ["_4"], ["_10"]));
     processDefinition.addFlowObject(new BPMNEndEvent("_5", "MyEnd", "endEvent", ["_8"], []));
     processDefinition.addFlowObject(boundaryEvent);
+    processDefinition.addFlowObject(new BPMNEndEvent("_9", "MyEnd2", "endEvent", ["_8"], []));
     processDefinition.addSequenceFlow(new BPMNSequenceFlow("_4", null, "sequenceFlow", "_2", "_3"));
     processDefinition.addSequenceFlow(new BPMNSequenceFlow("_8", null, "sequenceFlow", "_7", "_5"));
+    processDefinition.addSequenceFlow(new BPMNSequenceFlow("_10", null, "sequenceFlow", "_3", "_9"));
 
     processDefinition.attachBoundaryEvents();
 
-    var timeout = 1000;
+    var bpmnProcess;
 
     var handler = {
         "MyStart": function(data, done) {
@@ -37,7 +34,7 @@ exports.testBPMNTimeout = function(test) {
                         "position": "MyStart"
                     }
                 ],
-                "testBPMNTimeout: state at MyStart"
+                "testClearBPMNTimeoutByLeavingTask: state at MyStart"
             );
             done(data);
         },
@@ -49,53 +46,60 @@ exports.testBPMNTimeout = function(test) {
                         "position": "MyTask"
                     }
                 ],
-                "testBPMNTimeout: state at MyTask"
+                "testClearBPMNTimeoutByLeavingTask: state at MyTask"
+            );
+            this.data = {myproperty: "blah"};
+            done(data);
+        },
+        "MyTaskDone": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyTask"
+                    }
+                ],
+                "testClearBPMNTimeoutByLeavingTask: state at MyTask"
             );
             this.data = {myproperty: "blah"};
             done(data);
         },
         "MyTimeout:getTimeout": function() {
-            test.ok(true, "testBPMNTimeout: getTimout has been called");
-            return timeout;
+            test.ok(true, "testClearBPMNTimeoutByLeavingTask: getTimout has been called");
+
+            process.nextTick(function() {
+                bpmnProcess.taskDone("MyTask");
+            });
+            return 10000;
         },
         "MyTimeout": function(data, done) {
-            var activeTimers = bpmnProcess.activeTimers;
-            test.deepEqual(activeTimers.MyTimeout._idleTimeout,
-                timeout,
-                "testClearBPMNTimeoutByLeavingTask: active timers should be empty"
-            );
-
-            var history = this.getHistory();
-            test.deepEqual(history,
-                [
-                    "MyStart",
-                    "MyTask",
-                    "MyTimeout"
-                ],
-                "testBPMNTimeout: history at MyTimeout"
-            );
-
+            test.ok(false, "testClearBPMNTimeoutByLeavingTask: should never be here");
             done(data);
         },
-        "MyEnd": function(data, done) {
+        "MyEnd2": function(data, done) {
+
+            var activeTimers = bpmnProcess.activeTimers;
+            test.deepEqual(activeTimers,
+                {},
+                "testClearBPMNTimeoutByLeavingTask: active timers should be empty"
+            );
             var state = this.getState();
             test.deepEqual(state.tokens,
                 [
                     {
-                        "position": "MyEnd"
+                        "position": "MyEnd2"
                     }
                 ],
-                "testBPMNTimeout: state at MyEnd"
+                "testClearBPMNTimeoutByLeavingTask: state at MyEnd"
             );
             var history = this.getHistory();
             test.deepEqual(history,
                 [
                     "MyStart",
                     "MyTask",
-                    "MyTimeout",
-                    "MyEnd"
+                    "MyEnd2"
                 ],
-                "testBPMNTimeout: history at MyEnd"
+                "testClearBPMNTimeoutByLeavingTask: history at MyEnd"
             );
             done(data);
 
@@ -103,7 +107,7 @@ exports.testBPMNTimeout = function(test) {
         }
     };
 
-    var bpmnProcess = new BPMNProcess("myFirstProcess", processDefinition, handler);
+    bpmnProcess = new BPMNProcess("myFirstProcess", processDefinition, handler);
 
     bpmnProcess.sendStartEvent("MyStart");
 
