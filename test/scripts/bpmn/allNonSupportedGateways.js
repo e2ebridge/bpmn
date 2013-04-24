aba38d432ebb36ba52ab60f87a61cd7604c20f71
Marcel Rassinger <mrassinger@e2e.ch>
2013-04-24 08:44:17 +0200

    Test: parse supported gateways, throw errors for non-supported gateways.

---
+++ test/scripts/bpmn/allNonSupportedGateways.js
@@ -0,0 +1,45 @@
+/**
+ * AUTHOR: mrassinger
+ * COPYRIGHT: E2E Technologies Ltd.
+ */
+
+var bpmnParserModule = require('../../../lib/bpmn/parser.js');
+var errorQueueModule = require("../../../lib/errors.js");
+
+exports.testParseBPMNAllNonSupportedGateways = function(test) {
+
+    var errorQueue = errorQueueModule.createErrorQueue();
+    bpmnParserModule.parse("test/resources/bpmn/allNonSupportedGateways.bpmn", errorQueue);
+
+    var errors = errorQueue.getErrors();
+    test.deepEqual(errors,
+        [
+            {
+                "code": "UnsupportedGateway",
+                "fileName": "test/resources/bpmn/allNonSupportedGateways.bpmn",
+                "description": "The gateway 'Inclusive Gateway' is not supported yet."
+            },
+            {
+                "code": "UnsupportedGateway",
+                "fileName": "test/resources/bpmn/allNonSupportedGateways.bpmn",
+                "description": "The gateway 'Complex Gateway' is not supported yet."
+            },
+            {
+                "code": "UnsupportedGateway",
+                "fileName": "test/resources/bpmn/allNonSupportedGateways.bpmn",
+                "description": "The gateway 'Event Gateway' is not supported yet."
+            },
+            {
+                "code": "UnsupportedGateway",
+                "fileName": "test/resources/bpmn/allNonSupportedGateways.bpmn",
+                "description": "The gateway 'Event Gateway (Instantiate)' is not supported yet."
+            },
+            {
+                "code": "UnsupportedGateway",
+                "fileName": "test/resources/bpmn/allNonSupportedGateways.bpmn",
+                "description": "The gateway 'Parallel Event Gateway (Instantiate)' is not supported yet."
+            }
+        ],
+        "testParseBPMNAllNonSupportedGateways");
+    test.done();
+};
---
+++ test/scripts/bpmn/allSupportedGateways.js
@@ -0,0 +1,49 @@
+/**
+ * AUTHOR: mrassinger
+ * COPYRIGHT: E2E Technologies Ltd.
+ */
+
+var bpmnParserModule = require('../../../lib/bpmn/parser.js');
+
+exports.testParseBPMNAllSupportedGateways = function(test) {
+
+    var bpmnProcessDefinitions = bpmnParserModule.parse("test/resources/bpmn/allSupportedGateways.bpmn");
+    test.deepEqual(bpmnProcessDefinitions,
+        [
+            {
+                "bpmnId": "PROCESS_1",
+                "name": "AllSupportedGateways",
+                "flowObjects": [
+                    {
+                        "bpmnId": "_2",
+                        "name": "Exclusive Gateway",
+                        "type": "exclusiveGateway",
+                        "isFlowObject": true,
+                        "isExclusiveGateway": true
+                    },
+                    {
+                        "bpmnId": "_3",
+                        "name": "Exclusive Gateway",
+                        "type": "exclusiveGateway",
+                        "isFlowObject": true,
+                        "isExclusiveGateway": true
+                    },
+                    {
+                        "bpmnId": "_4",
+                        "name": "Parallel Gateway",
+                        "type": "parallelGateway",
+                        "isFlowObject": true,
+                        "isParallelGateway": true
+                    }
+                ],
+                "sequenceFlows": [],
+                "processElementIndex": null,
+                "sequenceFlowBySourceIndex": null,
+                "sequenceFlowByTargetIndex": null,
+                "boundaryEventsByAttachmentIndex": null,
+                "nameMap": null
+            }
+        ],
+        "testParseBPMNAllSupportedGateways");
+    test.done();
+};
