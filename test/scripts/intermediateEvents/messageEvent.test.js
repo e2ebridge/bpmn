dd8dd3e9301549f854560d394846faa6dc14faf0
Marcel Rassinger <mrassinger@e2e.ch>
2013-04-24 07:46:27 +0200

    Test: intermediate throw event.

---
+++ test/scripts/intermediateEvents/messageEvent.test.js
@@ -0,0 +1,92 @@
+/**
+ * AUTHOR: mrassinger
+ * COPYRIGHT: E2E Technologies Ltd.
+ */
+
+var bpmnProcessModule = require('../../../lib/process.js');
+var BPMNProcessDefinition = require('../../../lib/bpmn/processDefinition.js').BPMNProcessDefinition;
+var BPMNTask = require("../../../lib/bpmn/tasks.js").BPMNTask;
+var BPMNStartEvent = require("../../../lib/bpmn/startEvents.js").BPMNStartEvent;
+var BPMNEndEvent = require("../../../lib/bpmn/endEvents.js").BPMNEndEvent;
+var BPMNSequenceFlow = require("../../../lib/bpmn/sequenceFlows.js").BPMNSequenceFlow;
+var BPMNExclusiveGateway = require("../../../lib/bpmn/gateways.js").BPMNExclusiveGateway;
+var BPMNIntermediateThrowEvent = require("../../../lib/bpmn/intermediateEvents.js").BPMNIntermediateThrowEvent;
+
+
+exports.testIntermediateThrowEvent = function(test) {
+    /** @type {BPMNProcessDefinition} */
+    var processDefinition = new BPMNProcessDefinition("PROCESS_1", "myProcess");
+    processDefinition.addFlowObject(new BPMNStartEvent("_2", "MyStart", "startEvent"));
+    processDefinition.addFlowObject(new BPMNIntermediateThrowEvent("_3", "Intermediate Message Event", "intermediateThrowEvent"));
+    processDefinition.addFlowObject(new BPMNEndEvent("_5", "MyEnd", "endEvent"));
+    processDefinition.addSequenceFlow(new BPMNSequenceFlow("_4", "flow1", "sequenceFlow", "_2", "_3"));
+    processDefinition.addSequenceFlow(new BPMNSequenceFlow("_6", "flow2", "sequenceFlow", "_3", "_5"));
+
+    var handler = {
+        "MyStart": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyStart",
+                        "substate": null,
+                        "owningProcessId": "myIntermediateThrowEventProcess"
+                    }
+                ],
+                "testIntermediateThrowEvent: state at MyStart"
+            );
+            done(data);
+        },
+        "Intermediate_Message_Event": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "Intermediate Message Event",
+                        "substate": null,
+                        "owningProcessId": "myIntermediateThrowEventProcess"
+                    }
+                ],
+                "testIntermediateThrowEvent: state at Intermediate Message Event"
+            );
+            this.data = {myproperty: "blah"};
+            done(data);
+        },
+        "MyEnd": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyEnd",
+                        "substate": null,
+                        "owningProcessId": "myIntermediateThrowEventProcess"
+                    }
+                ],
+                "testIntermediateThrowEvent: state at MyEnd"
+            );
+            var history = this.getHistory();
+            test.deepEqual(history.historyEntries,
+                [
+                    {
+                        "name": "MyStart"
+                    },
+                    {
+                        "name": "Intermediate Message Event"
+                    },
+                    {
+                        "name": "MyEnd"
+                    }
+                ],
+                "testIntermediateThrowEvent: history at MyEnd"
+            );
+            done(data);
+
+            test.done();
+        }
+    };
+
+    var bpmnProcess = bpmnProcessModule.createBPMNProcess4Testing("myIntermediateThrowEventProcess", processDefinition, handler);
+
+    bpmnProcess.sendStartEvent("MyStart");
+
+};
