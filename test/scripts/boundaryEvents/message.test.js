13c2dee93bded1fa1178228c97a370bf68836c50
Marcel Rassinger <mrassinger@e2e.ch>
2013-05-21 18:26:11 +0200

    Feature: intermediate message events

---
+++ test/scripts/boundaryEvents/message.test.js
@@ -0,0 +1,135 @@
+/**
+ * AUTHOR: mrassinger
+ * COPYRIGHT: E2E Technologies Ltd.
+ */
+
+var bpmnProcessModule = require('../../../lib/process.js');
+var BPMNProcessDefinition = require('../../../lib/bpmn/processDefinition.js').BPMNProcessDefinition;
+var BPMNTask = require("../../../lib/bpmn/tasks.js").BPMNTask;
+var BPMNStartEvent = require("../../../lib/bpmn/startEvents.js").BPMNStartEvent;
+var BPMNEndEvent = require("../../../lib/bpmn/endEvents.js").BPMNEndEvent;
+var BPMNSequenceFlow = require("../../../lib/bpmn/sequenceFlows.js").BPMNSequenceFlow;
+var BPMNBoundaryEvent = require("../../../lib/bpmn/boundaryEvents.js").BPMNBoundaryEvent;
+
+exports.testBPMNMessageBoundaryEvent = function(test) {
+    var bpmnProcess;
+
+    var boundaryEvent = new BPMNBoundaryEvent("_7", "MyMessageBoundaryEvent", "boundaryEvent", "_3");
+    boundaryEvent.isMessageEvent = true;
+
+    var myTask = new BPMNTask("_3", "MyTask", "task");
+
+    /** @type {BPMNProcessDefinition} */
+    var processDefinition = new BPMNProcessDefinition("PROCESS_1", "myProcess");
+    processDefinition.addFlowObject(new BPMNStartEvent("_2", "MyStart", "startEvent"));
+    processDefinition.addFlowObject(myTask);
+    processDefinition.addFlowObject(new BPMNEndEvent("_5", "MyEnd", "endEvent"));
+    processDefinition.addFlowObject(boundaryEvent);
+    processDefinition.addSequenceFlow(new BPMNSequenceFlow("_4", null, "sequenceFlow", "_2", "_3"));
+    processDefinition.addSequenceFlow(new BPMNSequenceFlow("_8", null, "sequenceFlow", "_7", "_5"));
+
+    var handler = {
+        "MyStart": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyStart",
+                        "owningProcessId": "myFirstProcess"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: state at MyStart"
+            );
+            done(data);
+        },
+        "MyTask": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyTask",
+                        "owningProcessId": "myFirstProcess"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: state at MyTask"
+            );
+
+            var boundaryEvents = bpmnProcess.processDefinition.getBoundaryEventsAt(myTask);
+            test.ok(boundaryEvents.length === 1,
+                "testBPMNMessageBoundaryEvent: at MyTask: there should be one boundary event"
+            );
+
+            bpmnProcess.sendEvent("MyMessageBoundaryEvent", {gugus: "blah"});
+            done(data);
+        },
+        "MyMessageBoundaryEvent": function(data, done) {
+
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyMessageBoundaryEvent",
+                        "owningProcessId": "myFirstProcess"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: state at MyEnd"
+            );
+
+            var history = this.getHistory();
+            test.deepEqual(history.historyEntries,
+                [
+                    {
+                        "name": "MyStart"
+                    },
+                    {
+                        "name": "MyTask"
+                    },
+                    {
+                        "name": "MyMessageBoundaryEvent"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: history at MyMessageBoundaryEvent"
+            );
+
+            done(data);
+        },
+        "MyEnd": function(data, done) {
+            var state = this.getState();
+            test.deepEqual(state.tokens,
+                [
+                    {
+                        "position": "MyEnd",
+                        "owningProcessId": "myFirstProcess"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: state at MyEnd"
+            );
+            var history = this.getHistory();
+            test.deepEqual(history.historyEntries,
+                [
+                    {
+                        "name": "MyStart"
+                    },
+                    {
+                        "name": "MyTask"
+                    },
+                    {
+                        "name": "MyMessageBoundaryEvent"
+                    },
+                    {
+                        "name": "MyEnd"
+                    }
+                ],
+                "testBPMNMessageBoundaryEvent: history at MyEnd"
+            );
+            done(data);
+
+            test.done();
+        }
+    };
+
+    bpmnProcess = bpmnProcessModule.createBPMNProcess4Testing("myFirstProcess", processDefinition, handler);
+
+    bpmnProcess.sendEvent("MyStart");
+
+};
---
+++ test/scripts/bpmn/messageBoundaryEvent.test.js
@@ -0,0 +1,79 @@
+/**
+ * AUTHOR: mrassinger
+ * COPYRIGHT: E2E Technologies Ltd.
+ */
+
+var bpmnParserModule = require('../../../lib/bpmn/parser.js');
+
+exports.testParseBPMNMessageBoundaryEvent = function(test) {
+
+    var bpmnProcessDefinitions = bpmnParserModule.parse("test/resources/bpmn/messageBoundaryEvent.bpmn");
+    test.equal(bpmnProcessDefinitions.length, 1, "testParseBPMNMessageBoundaryEvent: number of processDefinitions");
+    var bpmnProcessDefinition = bpmnProcessDefinitions[0];
+
+    test.deepEqual(bpmnProcessDefinition,
+        {
+            "bpmnId": "PROCESS_1",
+            "name": "MessageBoundaryEvent",
+            "flowObjects": [
+                {
+                    "bpmnId": "_2",
+                    "name": "MyStart",
+                    "type": "startEvent",
+                    "isFlowObject": true,
+                    "isStartEvent": true
+                },
+                {
+                    "bpmnId": "_3",
+                    "name": "MyTask",
+                    "type": "task",
+                    "isFlowObject": true,
+                    "isActivity": true,
+                    "isWaitActivity": true
+                },
+                {
+                    "bpmnId": "_5",
+                    "name": "MyEnd",
+                    "type": "endEvent",
+                    "isFlowObject": true,
+                    "isEndEvent": true
+                },
+                {
+                    "bpmnId": "_6",
+                    "name": "My Message Boundary Event",
+                    "type": "boundaryEvent",
+                    "isFlowObject": true,
+                    "isBoundaryEvent": true,
+                    "attachedToRef": "_3",
+                    "isMessageEvent": true
+                }
+            ],
+            "sequenceFlows": [
+                {
+                    "bpmnId": "_4",
+                    "name": null,
+                    "type": "sequenceFlow",
+                    "sourceRef": "_2",
+                    "targetRef": "_3",
+                    "isSequenceFlow": true
+                },
+                {
+                    "bpmnId": "_9",
+                    "name": null,
+                    "type": "sequenceFlow",
+                    "sourceRef": "_6",
+                    "targetRef": "_5",
+                    "isSequenceFlow": true
+                }
+            ],
+            "processElementIndex": null,
+            "sequenceFlowBySourceIndex": null,
+            "sequenceFlowByTargetIndex": null,
+            "boundaryEventsByAttachmentIndex": null,
+            "nameMap": null,
+            "isProcessDefinition": true,
+            "collaboratingParticipants": []
+        },
+        "testParseBPMNMessageBoundaryEvent");
+    test.done();
+};
