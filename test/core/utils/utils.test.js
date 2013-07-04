8ceb9f2541f6f04fc1885a5ae1da7c5cfd45c019
Marcel Rassinger <mrassinger@e2e.ch>
2013-07-04 10:32:25 +0200

    Feature: hash map.

--- test/core/utils/utils.test.js
+++ test/core/utils/utils.test.js
@@ -1,16 +1,51 @@
 /**
- * AUTHOR: mrassinger
- * COPYRIGHT: E2E Technologies Ltd.
+ * Copyright: E2E Technologies Ltd
  */
-var utilsModule = require('../../../lib/utils/utils.js');
+"use strict";
+
+var utils = require('../../../lib/utils/utils.js');
 
 exports.testToUpperCamelCase = function(test){
 
-    var result1 = utilsModule.toUpperCamelCase("an example");
+    var result1 = utils.toUpperCamelCase("an example");
     test.equal(result1,"AnExample", "testToUpperCamelCase: one blank");
 
-    var result2 = utilsModule.toUpperCamelCase("an    example");
+    var result2 = utils.toUpperCamelCase("an    example");
     test.equal(result2,"AnExample", "testToUpperCamelCase: many blanks");
 
     test.done();
 };
+
+
+exports.testHashMapCaseSensitive = function(test){
+
+    var hashMap = new utils.HashMap();
+    hashMap.set("prototype", {blah: "x"});
+    var value = hashMap.get("prototype");
+    test.deepEqual(value, { blah: 'x' }, "testHashMapCaseSensitive: set/get");
+
+    var keys = hashMap.getKeys();
+    test.deepEqual(keys,
+        [
+            "prototype"
+        ],
+        "testHashMapCaseSensitive: keys"
+    );
+
+    hashMap.clear();
+    var keysAfterClear = hashMap.getKeys();
+    test.deepEqual(keysAfterClear, [], "testHashMapCaseSensitive: keysAfterClear");
+
+    test.done();
+};
+
+exports.testHashMapNotCaseSensitive = function(test){
+
+    var hashMap = new utils.HashMap(false);
+    hashMap.set("prototype", {blah: "x"});
+    var value = hashMap.get("ProtoType");
+    test.deepEqual(value, { blah: 'x' }, "testHashMapCaseSensitive: set/get");
+
+    test.done();
+};
+
